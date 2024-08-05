ca267ac1b9ed47e0e927401441e174957cae7b31
acd-jake <67855941+acd-jake@users.noreply.github.com>
2024-08-05 10:58:17 +0200

    Update TalkingActorsApi.js
    reordered methods

--- scripts/TalkingActorsApi.js
+++ scripts/TalkingActorsApi.js
@@ -4,14 +4,17 @@ export class TalkingActorsApi {
     async textToSpeech(voiceId, text, settings, chatlog, chatMessagePromise) {
         game.talkingactors.connector.textToSpeech(voiceId, text, settings, chatlog, chatMessagePromise);
     }
-    getVoiceIdAndSettingsFromActor(speakerActor) {
-        return game.talkingactors.connector.getVoiceIdAndSettingsFromActor(speakerActor);
-    }
+
     tryGetSpeakerActorForNarratingActor() {
         return game.talkingactors.connector.tryGetSpeakerActorForNarratingActor();
     }
+
     tryGetStandardSpeakerActor(chatData) {
         return this.tryGetStandardSpeakerActor(chatData);
+    }
+
+    getVoiceIdAndSettingsFromActor(speakerActor) {
+        return game.talkingactors.connector.getVoiceIdAndSettingsFromActor(speakerActor);
     }
 
 }
